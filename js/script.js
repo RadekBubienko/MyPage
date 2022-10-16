0bf1ab6ea6625ec2f2cd11b8bb8a60d1993132e0
Radek Bubienko <radek@bubienko.pl>
2022-10-16 20:01:27 +0200

    corrected classes and js

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
-console.log("Wiadomość dla developerów - Miło mi, że odwiedzasz moją stronkę! To jest nowy commit w GIT");
+{
+    console.log("Wiadomość dla developerów - Miło mi, że odwiedzasz moją stronkę! To jest nowy commit w GIT");
 
-let button1 = document.querySelector(".js-button1");
-let button2 = document.querySelector(".js-button2");
-let myImg = document.querySelector(".js-myImg");
+    let button1 = document.querySelector(".js-button1");
+    let button2 = document.querySelector(".js-button2");
+    let myImg = document.querySelector(".js-myImg");
 
-button1.addEventListener("click", () => {
+    button1.addEventListener("click", () => {
 
-    button1.innerText === "Usuń zdjęcie"
-        ? button1.innerText = "Pokaż moje zdjęcie"
-        : button1.innerText = "Usuń zdjęcie";
+        button1.innerText === "Usuń zdjęcie"
+            ? button1.innerText = "Pokaż moje zdjęcie"
+            : button1.innerText = "Usuń zdjęcie";
 
-    myImg.classList.toggle("displayNone");
-});
+        myImg.classList.toggle("displayNone");
+    });
 
-button2.addEventListener("click", () => {
-    dogImg = document.querySelector(".js-dogImg");
-    dogImg.classList.toggle("bigger");
-    button2.remove();
-    treat = document.querySelector(".js-treat");
-    treat.classList.add("warning");
-    treat.innerText = "Tofik dziękuje!";
-});
+    button2.addEventListener("click", () => {
+        dogImg = document.querySelector(".js-dogImg");
+        dogImg.classList.toggle("bigger");
+        button2.remove();
+        treat = document.querySelector(".js-treat");
+        treat.classList.add("warning");
+        treat.innerText = "Tofik dziękuje!";
+    });
 
-let buttonChangeTheme = document.querySelector(".js-buttonChangeTheme");
-let body = document.querySelector(".body");
-let header2 = document.querySelector(".js-header2");
-let header1 = document.querySelector(".js-header1");
-let paragraph = document.querySelector(".js-paragraph");
+    let buttonChangeTheme = document.querySelector(".js-buttonChangeTheme");
+    let body = document.querySelector(".body");
+    let aside = document.querySelector(".js-asideParagraph");
+    const headerElements = document.querySelectorAll(".js-header");
+    const paragraphElements = document.querySelectorAll(".js-paragraph");
 
-buttonChangeTheme.addEventListener("click", () => {
+    buttonChangeTheme.addEventListener("click", () => {
 
-    buttonChangeTheme.innerText === "Zmień motyw na ciemny"
-        ? buttonChangeTheme.innerText = "Zmień motyw na jasny"
-        : buttonChangeTheme.innerText = "Zmień motyw na ciemny";
-    body.classList.toggle("dark");
+        buttonChangeTheme.innerText === "Zmień motyw na ciemny"
+            ? buttonChangeTheme.innerText = "Zmień motyw na jasny"
+            : buttonChangeTheme.innerText = "Zmień motyw na ciemny";
+        body.classList.toggle("dark");
 
-    const headerElements = document.querySelectorAll(".js-header1")
-    headerElements.forEach(header1 => {
-    header1.classList.toggle("brighter")});
+        aside.classList.toggle("paragraph-brighter");
 
-    const subHeaderElements = document.querySelectorAll(".js-header2")
-    subHeaderElements.forEach(header2 => {
-        header2.classList.toggle("brighter")});
+        headerElements.forEach(header => {
+            header.classList.toggle("brighter")
+        });
 
-    const paragraphElements = document.querySelectorAll(".js-paragraph")
-    paragraphElements.forEach(paragraph => {
-        paragraph.classList.toggle("paragraph-brighter")}); 
-    
-});
+        paragraphElements.forEach(paragraph => {
+            paragraph.classList.toggle("paragraph-brighter")
+        });
+
+    });
+}
