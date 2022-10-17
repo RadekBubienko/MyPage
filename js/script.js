cf4a9e3e024deddf2311a1f09ed4ff7fa6a2e2c7
Radek Bubienko <radek@bubienko.pl>
2022-10-17 21:52:52 +0200

    added function in script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,39 @@
 {
     console.log("Wiadomość dla developerów - Miło mi, że odwiedzasz moją stronkę!");
 
-    let button1 = document.querySelector(".js-button1");
-    let button2 = document.querySelector(".js-button2");
-    let myImg = document.querySelector(".js-myImg");
+    const deleteMyPhoto = () => {
 
-    button1.addEventListener("click", () => {
-
+        const button1 = document.querySelector(".js-button1");
+        const myImg = document.querySelector(".js-myImg");
+        
         button1.innerText === "Usuń zdjęcie"
             ? button1.innerText = "Pokaż moje zdjęcie"
             : button1.innerText = "Usuń zdjęcie";
+    
+            myImg.classList.toggle("displayNone");
+    };
 
-        myImg.classList.toggle("displayNone");
-    });
+    const deleteDogPhoto = () => {
 
-    button2.addEventListener("click", () => {
+        const button2 = document.querySelector(".js-button2");
         dogImg = document.querySelector(".js-dogImg");
         dogImg.classList.toggle("bigger");
         button2.remove();
         treat = document.querySelector(".js-treat");
         treat.classList.add("warning");
         treat.innerText = "Tofik dziękuje!";
-    });
 
-    let buttonChangeTheme = document.querySelector(".js-buttonChangeTheme");
-    let body = document.querySelector(".body");
-    let aside = document.querySelector(".js-asideParagraph");
-    const headerElements = document.querySelectorAll(".js-header");
-    const paragraphElements = document.querySelectorAll(".js-paragraph");
 
-    buttonChangeTheme.addEventListener("click", () => {
+        button2.addEventListener("click", deleteDogPhoto );
+    }
+
+    const toggleTheme = () => {
+
+        const buttonChangeTheme = document.querySelector(".js-buttonChangeTheme");
+        const body = document.querySelector(".body");
+        const aside = document.querySelector(".js-asideParagraph");
+        const headerElements = document.querySelectorAll(".js-header");
+        const paragraphElements = document.querySelectorAll(".js-paragraph");
 
         buttonChangeTheme.innerText === "Zmień motyw na ciemny"
             ? buttonChangeTheme.innerText = "Zmień motyw na jasny"
@@ -44,7 +48,21 @@
 
         paragraphElements.forEach(paragraph => {
             paragraph.classList.toggle("paragraph-brighter")
-        });
+        });       
+       
+    };
+    
+    const init = () => {
+        const buttonChangeTheme = document.querySelector(".js-buttonChangeTheme");
+        buttonChangeTheme.addEventListener("click", toggleTheme);
 
-    });
+        const button1 = document.querySelector(".js-button1");
+        button1.addEventListener("click", deleteMyPhoto);
+
+        const button2 = document.querySelector(".js-button2");
+        button2.addEventListener("click", deleteDogPhoto );
+
+    }; 
+
+    init();
 }
